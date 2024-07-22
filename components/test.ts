eaf896393614f210fdab3351bed2d43dfe7a75c4
Igor Kirichenko <igor.kirichenko@dsr-corporation.com>
2024-07-22 15:33:11 +0100

    release(module): upgrade to angular v15

--- components/test.ts
+++ components/test.ts
@@ -11,8 +11,6 @@ import {
 
 // tslint:disable-next-line:no-any
 declare const __karma__: any;
-// tslint:disable-next-line:no-any
-declare const require: any;
 
 const tags = __karma__.config.args[0];
 // First, initialize the Angular testing environment.
@@ -32,9 +30,6 @@ if (tags && tags.indexOf('--') === -1) {
 } else {
   filterRegExp = /\.spec\.ts$/;
 }
-
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
 // Filter specify file
 const specFiles = context.keys().filter((path: string) => filterRegExp.test(path));
 // And load the modules.
